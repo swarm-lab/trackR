014baec1e7bb2556280b394fda19d5561d5fcda2
Simon Garnier <garnier@njit.edu>
2019-03-11 17:09:54 -0400

    Removes unnecessary slider

--- inst/trackR_quanta/UI/blob.R
+++ inst/trackR_quanta/UI/blob.R
@@ -4,8 +4,6 @@ bsCollapsePanel(
   htmlOutput("backgroundStatusBlob", container = p, class = "good"),
   sliderInput("blobBlur", "Smoothing factor:", min = 0, max = 10,
               value = 3, width = "100%"),
-  sliderInput("blobThreshold", "Detection threshold:", min = 1, max = 255,
-              value = 50, width = "100%"),
   sliderInput("blobSize", "Size range:", min = 1, max = 10000, value = c(1, 10000),
               width = "100%"),
   actionButton("recordBlobs", "Record blobs", width = "100%")

4c90263be1ab721d782b049823ce059b8f156455
Simon Garnier <garnier@njit.edu>
2019-03-19 23:11:08 +0100

    Adds basic trackPlayer function

--- R/trackR.R
+++ R/trackR.R
@@ -72,3 +72,49 @@ trackR <- function(tracker = "classic", ...) {
 
   shiny::runApp(app_path, ...)
 }
+
+
+#' @export
+trackPlayer <- function(video, tracks, delay = 10, save = FALSE) {
+  vid <- Rvision::video(video)
+  df <- readr::read_csv(tracks)
+  cbPalette <- c("#FFBF80", "#FF8000", "#FFFF99", "#FFFF33", "#B2FF8C", "#33FF00",
+                 "#A6EDFF", "#1AB2FF", "#CCBFFF", "#664CFF", "#FF99BF", "#E61A33")
+
+  if (save)
+    vw <- Rvision::videoWriter(paste0(sub(basename(video), "", video), "tracks.", tools::file_ext(video)),
+                               codec(vid), fps(vid), nrow(vid), ncol(vid))
+
+  newDisplay("trackPlayer", nrow(vid), ncol(vid))
+
+  for (i in 1:nframes(vid)) {
+    idx <- df$frame > (i - Rvision::fps(vid)) & df$frame <= i
+    sub_df <- df[idx, ]
+    frame <- readNext(vid)
+
+    for (j in unique(sub_df$track)) {
+      idx <- sub_df$track == j
+
+      if (length(sub_df[idx, ]$x) > 1) {
+        Rvision::drawLine(frame, sub_df[idx, ]$x[1:(length(sub_df[idx, ]$x) - 1)],
+                          sub_df[idx, ]$y[1:(length(sub_df[idx, ]$x) - 1)],
+                          sub_df[idx, ]$x[2:length(sub_df[idx, ]$x)],
+                          sub_df[idx, ]$y[2:length(sub_df[idx, ]$x)],
+                          cbPalette[(sub_df[idx, ]$track %% 12) + 1],
+                          round(ncol(frame) / 200))
+      } else {
+        Rvision::drawCircle(frame, sub_df[idx, ]$x, sub_df[idx, ]$y, 2, sub_df[idx, ]$track, 2)
+      }
+    }
+
+    Rvision::display(frame, "trackPlayer", 1, nrow(vid), ncol(vid))
+
+    if (save)
+      writeFrame(vw, frame)
+  }
+
+  destroyDisplay("trackPlayer")
+
+  if (save)
+    release(vw)
+}
